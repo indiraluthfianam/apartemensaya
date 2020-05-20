974443e49104344636b5ea4b2a033b201d37cf23
indiraluthfianam <65643975+indiraluthfianam@users.noreply.github.com>
2020-05-20 15:41:24 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# apartemensaya
+# Flask app for Titanic: Machine Learning from Disaster
+**- Gaurav Modi**<br>
+_(last edit: 04/15/2018)_
+<p>This is a flask app to predict whether a particular passenger would survive. Along with the label, it shows the Survival probability too.
+
+## Packages Used:
+```
++ Json
++ Pandas
++ Numpy
++ Flask
++ WTForms
++ Scikit-Learn
+```
+
+## Steps:
+### Data preperation and model building
++ Reading data
++ Splitting the data into training and testing data
++ Imputing missing values from training dataset.
++ Using values imputed from training data to fill missing values in testing dataset to avoid data leakage.
++ Encoding Categorical predictors using sklearn LabelEncoder
++ Fitting RandomForestClassifier with Training data
++ Serializing the model to disk
+
+### Prediction on flask app
+```
++ Navigate to prediction page - http://localhost:5000/predict.
++ User fill the form in the browser with required predictors value and then submit the form.
++ Values provided by users are put into model to get predictions.
++ The result from the model is showed to user on browser.
+```
+
+## To run the app
++ Open terminal in main directory '/Titanic Flask App'.
++ Then run app.py from terminal
+
+```
+python app.py
+```
+This will start flask app server. Now can open the site by opening http://localhost:5000<br>
+Note: Right now, app runs in debug mode
+
+## Future work
++ Improve the model
++ Provide drop down for user input
++ Take user input as string for categorical data, instead of current method i.e. encoded numeric values.
++ Improve the UI and UX.
---
+++ app.py
@@ -0,0 +1,63 @@
+# importing the packages
+import json
+import pickle
+# import pandas as pd
+import numpy as np
+from flask import Flask, request, jsonify, render_template, flash, redirect, url_for, session, logging
+from sklearn.externals import joblib
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+
+# value of __name__ should be  '__main__'
+app = Flask(__name__)
+# Loading model so that it works on production
+model = joblib.load('./model/model.pkl')
+
+
+@app.route('/')
+def index():
+	# Index page
+	return render_template('index.html')
+
+@app.route('/about')
+def about():
+	# about me page
+	return render_template('about.html')
+
+@app.route('/products')
+def PRODUCTS():
+    """ Main page of the API """
+    return render_template('products.html')
+
+@app.route('/store')
+def STORE():
+    """ Main page of the API """
+    return render_template('store.html')
+
+@app.route('/RF')
+def RF():
+    """ Main page of the API """
+    return render_template('predict.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+	'''
+	For rendering results on HTML GUI
+	'''
+	int_features = [float(x) for x in request.form.values()]
+	final_features = [np.array(int_features)]
+	prediction = model.predict(final_features)
+	output = prediction[0]
+	return render_template('result.html', prediction_text='Rp {}'.format(output))
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+	'''
+	For direct API calls trought request
+	'''
+	data = request.get_json(force=True)
+	prediction = model.predict([np.array(list(data.values()))])
+	output = prediction[0]
+	return jsonify(output)
+if __name__ == "__main__":
+    app.run(debug=True)
